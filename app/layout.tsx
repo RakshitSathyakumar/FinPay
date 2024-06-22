5541e75c2fd77a4a3c9d80aef9579d35b84294e3
rakshit <rakshit.2002.rc@gmail.com>
2024-06-22 11:59:21 +0530

    change layout.tsx for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 
 export const dynamic = "force-dynamic";
 
@@ -16,50 +16,50 @@ const ibmPlexSerif = IBM_Plex_Serif({
   variable: "--font-ibm-plex-serif",
 });
 
-// export const metadata: Metadata = {
-//   title: "FinPay",
-//   description: "FinPay is a modern banking platform for everyone.",
-//   icons: {
-//     icon: "/icons/logo.svg",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "FinPay",
+  description: "FinPay is a modern banking platform for everyone.",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
-declare const window: any;
+// declare const window: any;
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const initBotpress = () => {
-    window.botpressWebChat.init({
-      composerPlaceholder: "Chat with bot",
-      botConversationDescription:
-        "This chatbot was built surprisingly fast with Botpress",
-      botId: "0f184fd4-fb3d-456b-91e0-2eb2c686f07e",
-      hostUrl: "https://cdn.botpress.cloud/webchat/v1",
-      messagingUrl: "https://messaging.botpress.cloud",
-      clientId: "0f184fd4-fb3d-456b-91e0-2eb2c686f07e",
-      webhookId: "c742b92c-26d7-4594-a023-0f06d99c7bac",
-      lazySocket: true,
-      themeName: "prism",
-      frontendVersion: "v1",
-      showPoweredBy: true,
-      theme: "prism",
-      themeColor: "#2563eb",
-      allowedOrigins: [],
-    });
-  };
+  // const initBotpress = () => {
+  //   window.botpressWebChat.init({
+  //     composerPlaceholder: "Chat with bot",
+  //     botConversationDescription:
+  //       "This chatbot was built surprisingly fast with Botpress",
+  //     botId: "0f184fd4-fb3d-456b-91e0-2eb2c686f07e",
+  //     hostUrl: "https://cdn.botpress.cloud/webchat/v1",
+  //     messagingUrl: "https://messaging.botpress.cloud",
+  //     clientId: "0f184fd4-fb3d-456b-91e0-2eb2c686f07e",
+  //     webhookId: "c742b92c-26d7-4594-a023-0f06d99c7bac",
+  //     lazySocket: true,
+  //     themeName: "prism",
+  //     frontendVersion: "v1",
+  //     showPoweredBy: true,
+  //     theme: "prism",
+  //     themeColor: "#2563eb",
+  //     allowedOrigins: [],
+  //   });
+  // };
 
   return (
     <html lang="en">
       <head>
-        <Script
+        {/* <Script
           src="https://cdn.botpress.cloud/webchat/v0/inject.js"
           onLoad={() => {
             initBotpress();
           }}
-        />
+        /> */}
       </head>
       <body className={inter.className}>{children}</body>
     </html>
